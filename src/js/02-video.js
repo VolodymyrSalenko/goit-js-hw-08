1da08c088b0e62216b6f5d570ef3a01a64a9417e
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-09 03:01:18 +0200

    Task2_1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from "@vimeo/player";
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function(data) {
+    console.log('data', data);
+});
+
+
+
+// Зберігай час відтворення у локальне сховище.Нехай ключем для сховища 
+// буде рядок "videoplayer-current-time".
+
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з 
+// метою відновлення відтворення зі збереженої позиції.
+
+// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час 
+// відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
