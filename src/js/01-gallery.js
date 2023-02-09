b27ebdaeb49b0a9fa5bb7807fb64f34b2b9a93c2
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-09 02:21:43 +0200

    Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryContent = galleryItems.map(({ preview, original, description }) => {
+return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryContent);
+
+
+const gallery = new SimpleLightbox('.gallery a', {   
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
